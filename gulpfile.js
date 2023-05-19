f375924625680e15ca2d2ae6cc01459e3080761e
Semyon Gnedin <gnedin.sema@mail.ru>
2023-05-19 17:04:27 +0500

    Правки

--- gulpfile.js
+++ gulpfile.js
@@ -99,9 +99,16 @@ const sprite = () => {
 
 const copy = (done) => {
   gulp
-    .src(["source/fonts/*.{woff2,woff}", "source/*.ico"], {
-      base: "source",
-    })
+    .src(
+      [
+        "source/fonts/*.{woff2,woff}",
+        "source/*.ico",
+        "source/manifest.webmanifest",
+      ],
+      {
+        base: "source",
+      }
+    )
     .pipe(gulp.dest("build"));
   done();
 };
