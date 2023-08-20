8d05f2d2ad13833635a4142c61403400ff9b548f
Apple <apple@Apple-4.local>
2023-08-21 01:30:07 +0500

    Правки - финал

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,8 @@
+let header = document.querySelector(".header");
 let navMain = document.querySelector(".header__nav");
 let navToggle = document.querySelector(".header__button");
 
-navMain.classList.remove("header__nav--nojs");
+header.classList.remove("header--nojs");
 navMain.classList.add("header-nav--closed");
 
 navToggle.addEventListener("click", function () {
